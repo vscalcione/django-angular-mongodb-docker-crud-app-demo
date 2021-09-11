d8b90ae3bc8b0b30433366e73ea627a6b72799de
vscalcione <vincenzo99.scalcione@gmail.com>
2021-09-11 13:33:07 +0200

    Updated requirements.txt file

--- django-mongodb-crud-app-demo/djangorestapi/djangorestapi/settings.py
+++ django-mongodb-crud-app-demo/djangorestapi/djangorestapi/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'tutorials.app.TutorialsConfig',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
