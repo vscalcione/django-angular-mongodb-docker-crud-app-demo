eb5547804c22a222bce800e488a7db37f7b56988
vscalcione <vincenzo99.scalcione@gmail.com>
2021-09-11 13:30:18 +0200

    Added subproject backend

--- django-mongodb-crud-app-demo/djangorestapi/djangorestapi/settings.py
+++ django-mongodb-crud-app-demo/djangorestapi/djangorestapi/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'djangorestapi.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default:': {
+    'default': {
         'ENGINE': 'djongo',
         'NAME': 'djangomongo_db',
         'HOST': '127.0.0.1',
