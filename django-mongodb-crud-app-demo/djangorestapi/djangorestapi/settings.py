7b7c948defe172d554340598b87ae934813b1498
vscalcione <vincenzo99.scalcione@gmail.com>
2021-09-11 13:44:37 +0200

    Added model

--- django-mongodb-crud-app-demo/djangorestapi/djangorestapi/settings.py
+++ django-mongodb-crud-app-demo/djangorestapi/djangorestapi/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'tutorials.app.TutorialsConfig',
+    'tutorials.apps.TutorialsConfig',
     'corsheaders'
 ]
 
@@ -50,7 +50,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware'
 ]
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8081',
+)
 
 ROOT_URLCONF = 'djangorestapi.urls'
 
